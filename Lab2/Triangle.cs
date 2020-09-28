31abd7532f7114e974e65a11a28ee1dfe9339914
Jessica0192 <jessica012125@gmail.com>
2020-09-29 04:33:29 +0900

    CalculateHypotenuse method meets the test

--- Lab2/Triangle.cs
+++ Lab2/Triangle.cs
@@ -8,7 +8,9 @@ namespace Lab2
     {
         public double CalculateHypotenuse(double A, double B)
         {
-            return 1;
+            double hypo = Math.Sqrt(Math.Pow(A, 2) + Math.Pow(B, 2));
+
+            return hypo;
         }
     }
 }
